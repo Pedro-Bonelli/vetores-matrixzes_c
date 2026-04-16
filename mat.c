631673e845466b883ab98d6a5fd415c2c56ff16a
Pedro-Bonelli <bonellip9@gmail.com>
2026-04-16 17:16:20 -0300

    Execício vetor e matriz

---
+++ mat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+    int m[3][4], v[3], somav;
+
+    //Ler os valores da matriz
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++){
+            printf("Valor: ");
+            scanf("%d", &m[i][j]);
+        }
+    }
+
+    // Imprimir a matriz
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++){
+            printf("%d\t", m[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    // Somar as linhas das matrizes
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 4; j++){
+            v[i] += m[i][j]; // Soma dos valores na mesma linha 
+        }
+
+        printf("\n");
+    }
+
+    // Imprimir os elementos do vetor
+    for (int i = 0; i < 3; i++){
+        printf("%d\t", v[i]);
+    }
+
+    // Somar os elementos do vetor
+    for (int i = 0; i < 3; i++){
+        somav += v[i];
+    }
+
+    // Imprimir a soma dos elementos do vetor
+    printf("%d", somav);
+}
